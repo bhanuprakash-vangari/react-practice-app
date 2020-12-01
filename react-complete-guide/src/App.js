ccd81f85a14e641c6666d94b49c0376abbe0c607
bhanu6143 <32790372+bhanu6143@users.noreply.github.com>
2020-12-01 09:48:30 +0530

    commit on 'master

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Bhanu", age: 23 },
+      { name: "Prakash", age: 21 },
+      { name: "Vangari", age: 32 },
+    ],
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My Hobbies are Coding
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
