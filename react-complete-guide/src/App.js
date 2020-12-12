d987bec88e91ab778cbde5bc38b7ee01e0fef8fd
bhanu6143 <32790372+bhanu6143@users.noreply.github.com>
2020-12-12 16:06:07 +0530

    styled-components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
-import Radium from 'radium';
 import Person from "./Person/Person";
+import styled from 'styled-components'
 
+
+const StyledButton = styled.button`
+  margin: auto;
+  box-shadow: 2px 5px #a0ff33;
+  padding: 7.5px 15px;
+  background-color: green;
+  border: 2px mediumblue;
+  color: white;
+  border: none;
+  outline: none;
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+    boxshadow: 2px 5px #b6ffaa;
+  }
+`;
 class App extends Component {
   state = {
     persons: [
@@ -30,19 +46,6 @@ class App extends Component {
     this.setState({ showPerson: !showStatus });
   };
   render() {
-    const buttonStyle = {
-      margin: "auto",
-      boxShadow: "2px 5px #A0FF33",
-      padding: "5px 10px",
-      backgroundColor: "green",
-      border: "2px mediumblue",
-      color: "white",
-      ":hover": {
-        backgroundColor: "lightgreen",
-        color: "black",
-        boxShadow: "2px 5px #B6FFAA",
-      },
-    };
     let persons = null;
     if (this.state.showPerson) {
       persons = (
@@ -60,25 +63,19 @@ class App extends Component {
           })}
         </div>
       );
-      buttonStyle.backgroundColor='blue'
-      buttonStyle.boxShadow ='2px 5px #AAFFE0'
-      buttonStyle[":hover"] = {
-        backgroundColor: "aquamarine",
-        color: "black",
-        boxShadow: "2px 5px #AAFFE0",
-      };
+     
     }
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is really working!</p>
-        <button style={buttonStyle} onClick={this.togglePersonHandler}>
+        <StyledButton onClick={this.togglePersonHandler}>
           Click Me
-        </button>
+        </StyledButton>
         {persons}
       </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,15 +1,39 @@
-import React from "react"
-import "./Person.css"
+import React from "react";
+import "./Person.css";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: auto ;
+  border: 1px solid #eeeeee;
+  box-shadow: 0 2px 3px aquamarine;
+  padding: 18px;
+  text-align: center;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 20px;
+  color: aliceblue;
+  background-color: blue;
+`;
+
+const StyledInput = styled.input`
+  padding: 8px;
+  outline-color: aquamarine;
+`;
 
 const person = (props) => {
+
   return (
-    <div className="Person">
+    <StyledDiv>
       <p onClick={props.click}>
         I'm {props.name}! My age is {props.age} and My Hobbies are{" "}
         {props.hobbies}
       </p>
-      <input className="input-text" type="text" onChange={props.changed} value={props.hobbies} />
-    </div>
-  )
-}
-export default person
+      <StyledInput
+        type="text"
+        onChange={props.changed}
+        value={props.hobbies}
+      />
+    </StyledDiv>
+  );
+};
+export default person;
